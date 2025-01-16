d18092b637a6ae72ed125dc883c77c7c22341a2e
VincentGoins <101417502+VincentGoins@users.noreply.github.com>
2025-01-16 13:29:21 -0500

    Initial Commit

---
+++ chatappTests/chatappTests.swift
@@ -0,0 +1,17 @@
+//
+//  chatappTests.swift
+//  chatappTests
+//
+//  Created by Vincent Goins on 1/16/25.
+//
+
+import Testing
+@testable import chatapp
+
+struct chatappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
